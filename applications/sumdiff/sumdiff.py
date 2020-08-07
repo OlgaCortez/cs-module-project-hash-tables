096e27ede4cd8550a7ead952ebd9c25e76726bfe
Olga Cortez <olgalataviacortez@gmail.com>
2020-08-06 20:02:09 -0500

    Met Day 4 MVP

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -13,3 +13,61 @@ def f(x):
 
 # Your code here
 
+# First Pass Solution
+
+def all_combos(numbers):
+    numbers = list(numbers)
+    plus = {}
+    minus = {}
+    for num in numbers:
+        answer = f(num)
+        f_answers[num] = answer
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            plus_value = f(numbers[i]) + f(numbers[j])
+            plus_key = (numbers[i], numbers[j])
+            plus[plus_key] = plus_value
+                
+            minus_value = f(numbers[i]) - f(numbers[j])
+            minus_key = (numbers[i], numbers[j])
+            minus[minus_key] = minus_value
+                
+    for (key1, value1) in plus.items():
+        for (key2, value2) in minus.items():
+            if plus[key1] is minus[key2]:
+                print(f"f({key1[0]}) + f({key1[1]}) = f({key2[0]}) - f({key2[1]})  ***  {f_answers[key1[0]]} + {f_answers[key1[1]]} = {f_answers[key2[0]]} - {f_answers[key2[1]]}  ***  {plus[key1]} = {minus[key2]}")
+
+def all_solutions(numbers):
+    numbers = list(numbers)
+    plus = {}
+    minus = {}
+    minus_list = []
+    count = 0
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            plus_value = f(numbers[i]) + f(numbers[j])
+            plus_key = (numbers[i], numbers[j])
+            plus[plus_key] = plus_value
+                
+            minus_key = f(numbers[i]) - f(numbers[j])
+            minus_value = (numbers[i], numbers[j])
+            if minus_key not in minus:
+                minus[minus_key] = [minus_value]
+            else:
+                minus[minus_key].append(minus_value)
+
+    for (key, value) in plus.items():
+            if value in minus:
+                for item in minus[value]:
+                    count += 1
+                    print(f"f({key[0]}) + f({key[1]}) = f({item[0]}) - f({item[1]})  ***  {f(key[0])} + {f(key[1])} = {f(item[0])} - {f(item[1])}")
+     
+    print(f"Solution Count: {count}")
+
+# all_combos(q)
+import time
+start_time = time.time()
+all_solutions(q)
+end_time = time.time()
+print(f"Second Pass Solution took: {end_time - start_time}")
+
